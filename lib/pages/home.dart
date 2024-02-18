8e1d470340c40ed65ff2d995e0c03cca12ebf1c2
Mezbaul Islam Zion <65642391+zionmezba@users.noreply.github.com>
2024-02-19 04:20:35 +0600

    home modification

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,15 +16,40 @@ class _HomeState extends State<Home> {
     print(data);
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          IconButton(
+          child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+        child: Column(
+          children: [
+            IconButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/location');
               },
               icon: Icon(Icons.edit_location),
-          )
-        ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  data['location'],
+                  style: const TextStyle(
+                    fontSize: 28,
+                    letterSpacing: 2.0,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 20,),
+            Text(
+              data['time'],
+              style: TextStyle(
+                fontSize: 66,
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
